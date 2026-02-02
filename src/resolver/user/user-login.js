f6b43fa637beaa52da14e584cdef0cd315982e21
25LP9922 25LP9922 <25LP9922@admins-iMac.local>
2026-02-02 14:28:31 +0800

    fix:status code user login

--- src/resolver/user/user-login.js
+++ src/resolver/user/user-login.js
@@ -3,13 +3,13 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 export const login = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   if (!email) return res.status(404).json("EMAIL IS MISSING");
   try {
     const user = await userModel.findOne({ email: email });
     if (!user) return res.status(404).json("USER NOT FOUND");
-    const result = await bcrypt.compare(email.password, user.password);
-    if (result === false) return res.status(500).json("AUTH FAILED");
+    const result = await bcrypt.compare(password, user.password);
+    if (result === false) return res.status(401).json("AUTH FAILED");
     if (result === true) {
       const token = jwt.sign(
         { email: user.email, id: user._id, role: user.role },
